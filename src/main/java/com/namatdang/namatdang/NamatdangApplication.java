b377adf464d7ccd951b4865ad01ede8747b02163
Sangwonee <sangw12477@gmail.com>
2026-08-12 09:50:27 +0900

    chore: 남았당 프로젝트 초기 설정
    Spring Boot 기본 구조와 설정을 추가하고 README 및 Git 제외 규칙을 작성한다.

---
+++ src/main/java/com/namatdang/namatdang/NamatdangApplication.java
@@ -0,0 +1,13 @@
+package com.namatdang.namatdang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NamatdangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NamatdangApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/namatdang/namatdang/NamatdangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.namatdang.namatdang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NamatdangApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
